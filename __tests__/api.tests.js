b2fd0005a24752c511d376c85de90350c33b65c9
Mikael <144145271+mikael-v@users.noreply.github.com>
2025-01-14 10:52:26 +0000

    fixes tests

--- __tests__/api.tests.js
+++ __tests__/api.tests.js
@@ -1,85 +1,58 @@
-const request = require("supertest"); 
-require("jest-sorted"); 
+const app = require("../app.js");
+const request = require("supertest");
+require("jest-sorted");
 const { endpoints } = require("../endpoints.json");
 
-const BASE_URL = "http://localhost:3000"; 
-
 describe("/api", () => {
-  test("Returns 200 status code and responds with an array containing the correct properties", async () => {
+  test("Returns 200 status code and responds with array containing the correct properties ", () => {
     jest.setTimeout(10000);
-    const response = await request(BASE_URL).get("/api").expect(200);
-    const result = response.body.endpoints;
-    const expected = endpoints;
-    expect(result).toEqual(expected);
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then((response) => {
+        const result = response.body.endpoints;
+        const expected = endpoints;
+        expect(result).toEqual(expected);
+      });
   });
 });
 
-describe("/api/artwork", () => {
-  test("Returns 200 and responds with an array of artworks with correct properties", async () => {
-    const response = await request(BASE_URL)
-      .get("/api/artwork?page=1&limit=10") 
-      .expect(200);
-
-    const artworks = response.body.records; 
-    expect(artworks.length).toBeLessThanOrEqual(10); 
-    artworks.forEach((artwork) => {
-      expect(artwork).toHaveProperty("id");
-      expect(artwork).toHaveProperty("title");
-      expect(artwork).toHaveProperty("artist");
-      expect(artwork).toHaveProperty("img_url");
-    });
+describe("/api/artworks", () => {
+  test("Returns 200 and responds with an array of artworks with correct properties", () => {
+    return request(app)
+      .get("/api/artworks?page=1&limit=10")
+      .expect(200)
+      .then((response) => {
+        const artworks = response.body.artworks;
+        expect(artworks.length).toBeLessThanOrEqual(20);
+      });
+  });
+  test("Returns 400 for invalid query parameters", () => {
+    return request(app)
+      .get("/api/artworks?page=abc&limit=xyz")
+      .expect(400)
+      .then((response) => {
+        expect(response.body.msg).toBe("Invalid query parameters");
+      });
   });
 });
 
-describe("/api/artwork/:id", () => {
-  test("Returns 200 and responds with an artwork object with correct properties", async () => {
-    const response = await request(BASE_URL)
-      .get("/api/artwork?id=O1013211") // Use `?id=O1013211` for serverless
-      .expect(200);
-
-    const artwork = response.body;
-    expect(artwork).toMatchObject({
-      id: "O1013211",
-      title: "Untitled",
-      artist: "Heussner & Co",
-      img_url:
-        "https://framemark.vam.ac.uk/collections/2016JK6699/full/!100,100/0/default.jpg",
-      medium: [
-        {
-          text: "pencil",
-          id: "x30347",
-        },
-        {
-          text: "ink",
-          id: "AAT15012",
-        },
-        {
-          text: "watercolour",
-          id: "x33202",
-        },
-      ],
-      techniques: [
-        {
-          text: "drawing",
-          id: "x32498",
-        },
-      ],
-      categories: [
-        {
-          text: "Wall coverings",
-          id: "THES48878",
-        },
-        {
-          text: "Designs",
-          id: "THES48968",
-        },
-        {
-          text: "Drawings",
-          id: "THES48966",
-        },
-      ],
-      dimensions: [],
-      source: "Victoria and Albert Museum",
-    });
+describe("/api/artworks/:id", () => {
+  test("Returns 200 and responds with an artwork with correct properties", () => {
+    return request(app)
+      .get("/api/artworks/5")
+      .expect(200)
+      .then((response) => {
+        const artwork = response.body;
+        expect(Object.keys(artwork).length).toBe(57);
+      });
+  });
+  test("Returns 400 for invalid query parameters", () => {
+    return request(app)
+      .get("/api/artworks/nonevalid")
+      .expect(400)
+      .then((response) => {
+        expect(response.body.msg).toBe("Invalid query parameters");
+      });
   });
 });
